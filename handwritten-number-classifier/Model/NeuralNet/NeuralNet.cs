0ccef063de442b8cbd3740ca8e095cfb232e877e
bjuan210302 <bjuan210302@gmail.com>
2021-05-07 21:32:06 -0500

    modifications + feedforward first ver

--- handwritten-number-classifier/Model/NeuralNet/NeuralNet.cs
+++ handwritten-number-classifier/Model/NeuralNet/NeuralNet.cs
@@ -1,49 +1,63 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using NumSharp;
 
 namespace handwritten_number_classifier.Model.NeuralNet
 {
     public class NeuralNet
     {
-        // Initially, the NN has 784 neurons as input, one hidden layer of 8 neurons and a output layer of 10 neurons
-        private int NUM_INPUT_TO_PROCESS = 1;
-        private int INPUT_SIZE = 784;
-        private int NEURONS_PER_LAYER = 8;
+        // Initially, the NN has 784 neurons as input, two hidden layers of 8 neurons and an output layer of 10 neurons
+        private const int InputSize = 784;
+        private const int NumOfLayers = 2; //Two hidden layers
+        private const int NeuronsPerLayer = 8;
+        private const int NeuronsOutLayer = 10;
         
-        private NDArray _x;
-        private NDArray _y;
-        private NDArray _biases;
-        private NDArray _weights;
+        private NDArray _layers;
+        private List<NDArray> _weights;
+        private List<NDArray> _biases;
 
         public NeuralNet()
         {
-            
-        }
-        public NeuralNet(NDArray x, NDArray y)
-        {
-            this._x = x;
-            this._y = y;
-        }
-        
-        public NeuralNet(String a)
-        {
+            _weights = new List<NDArray>();
+            _biases = new List<NDArray>();
             //Each neuron in the input has a weight pointing to the NEURONS_PER_LAYER neurons of the next layer
-            
-            _weights = np.random.rand((INPUT_SIZE, NEURONS_PER_LAYER));
+            //Same for the other layers
+            _weights.Add(np.random.rand((NeuronsPerLayer, InputSize))); //First layer weights
+            _weights.Add(np.random.rand((NeuronsPerLayer, NeuronsPerLayer))); //Second layer weights
+            _weights.Add(np.random.rand((NeuronsOutLayer, NeuronsPerLayer))); //Final layer weights
             
             //And the matrix multiplication of the input (NUM_INPUT_TO_PROCESS x INPUT_SIZE) * (INPUT_SIZE * NEURONS_PER_LAYER)
             // = (NUM_INPUT_TO_PROCESS x NEURONS_PER_LAYER)
             //Each one of those results needs to be summed with a bias integer
 
             //This [-10, 10] range for the biases is completely arbitrary
-            _biases = np.random.randint(-10, 10, (NUM_INPUT_TO_PROCESS, NEURONS_PER_LAYER));
+            _biases.Add(np.random.randint(-10, 10, (NeuronsPerLayer, 1)));
+            _biases.Add(np.random.randint(-10, 10, (NeuronsPerLayer, 1)));
+            _biases.Add(np.random.randint(-10, 10, (NeuronsOutLayer, 1)));
+            
+            //Finally an array to store layers values
+            _layers = np.zeros((NeuronsPerLayer, NumOfLayers));
         }
 
-        public NDArray ActivationFunction(NDArray matrix)
+        private NDArray ActivationFunction(NDArray matrix)
         {
             //The Sigmoid Function
             matrix = 1 / (1 + np.exp(-1*matrix));
             return matrix;
+        }
+
+        private NDArray FeedForward(NDArray input)
+        {
+            //This method returns the output vector 
+            //"input" must be a InputSize x 1 matrix
+            NDArray info = input;
+            for (int i = 0; i < NumOfLayers; i++)
+            {
+                _layers[i] = ActivationFunction(info.dot(_weights[i]) + _biases[i]); //Layer = WEIGHTS * INPUT + BIASES
+                info = _layers[i];
+            }
+            
+            return ActivationFunction(info.dot(_weights.Last()) + _biases.Last()); //Output vector
         }
     }
 }
