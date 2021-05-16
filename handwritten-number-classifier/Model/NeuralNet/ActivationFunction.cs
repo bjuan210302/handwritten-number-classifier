cf8beacc08c620c88309567874a850d23256ea4d
bjuan210302 <bjuan210302@gmail.com>
2021-05-16 17:52:20 -0500

    add: backprop, nn  now has only one hidden layer

--- handwritten-number-classifier/Model/NeuralNet/ActivationFunction.cs
+++ handwritten-number-classifier/Model/NeuralNet/ActivationFunction.cs
@@ -5,17 +5,21 @@ namespace handwritten_number_classifier.Model.NeuralNet
 {
     public static class ActivationFunction
     {
-        public static NDArray Compute(NDArray matrix)
+        public static NDArray ComputeSigmoid(NDArray matrix)
         {
             //The Sigmoid Function
             matrix = 1 / (1 + np.exp(-1*matrix));
             return matrix;
         }
 
+        public static NDArray ComputeRelu(NDArray matrix)
+        {
+            return np.maximum(matrix, 0);
+        }
         public static NDArray ComputeDerivative(NDArray matrix)
         {
-            //TODO
-            return null;
+            //Derivative of te sigmoid function
+            return matrix * (1 + -1*matrix); //For some reason "-matrix" is not a valid expression
         }
     }
 }
--- handwritten-number-classifier/Model/NeuralNet/NeuralNet.cs
+++ handwritten-number-classifier/Model/NeuralNet/NeuralNet.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using NumSharp;
 
 namespace handwritten_number_classifier.Model.NeuralNet
@@ -7,50 +6,64 @@ namespace handwritten_number_classifier.Model.NeuralNet
     public class NeuralNet
     {
         // Initially, the NN has 784 neurons as input, two hidden layers of 8 neurons and an output layer of 10 neurons
-        private const int InputSize = 784;
-        private const int NumOfLayers = 2; //Two hidden layers
         private const int NeuronsPerLayer = 8;
         private const int NeuronsOutLayer = 10;
         
-        private NDArray _layers;
         private List<NDArray> _weights;
         private List<NDArray> _biases;
+        private NDArray X;
+        private NDArray Y;
 
-        public NeuralNet()
+        public NeuralNet(NDArray x, NDArray y)
         {
+            this.X = x;
+            this.Y = y;
             _weights = new List<NDArray>();
             _biases = new List<NDArray>();
+            
             //Each neuron in the input has a weight pointing to the NEURONS_PER_LAYER neurons of the next layer
             //Same for the other layers
-            _weights.Add(np.random.rand((NeuronsPerLayer, InputSize))); //First layer weights
-            _weights.Add(np.random.rand((NeuronsPerLayer, NeuronsPerLayer))); //Second layer weights
-            _weights.Add(np.random.rand((NeuronsOutLayer, NeuronsPerLayer))); //Final layer weights
-            
-            //And the matrix multiplication of the input (NUM_INPUT_TO_PROCESS x INPUT_SIZE) * (INPUT_SIZE * NEURONS_PER_LAYER)
-            // = (NUM_INPUT_TO_PROCESS x NEURONS_PER_LAYER)
-            //Each one of those results needs to be summed with a bias integer
-
-            //This [-10, 10] range for the biases is completely arbitrary
-            _biases.Add(np.random.randint(-10, 10, (NeuronsPerLayer, 1)));
-            _biases.Add(np.random.randint(-10, 10, (NeuronsPerLayer, 1)));
-            _biases.Add(np.random.randint(-10, 10, (NeuronsOutLayer, 1)));
-            
-            //Finally an array to store layers values
-            _layers = np.zeros((NeuronsPerLayer, NumOfLayers));
+            _weights.Add(np.random.rand((NeuronsPerLayer, x.shape[0]))); //W1
+            _biases.Add(np.zeros((NeuronsPerLayer, 1))); //b1
+            _weights.Add(np.random.rand((NeuronsOutLayer, NeuronsPerLayer))); //W2
+            _biases.Add(np.zeros((NeuronsOutLayer, 1))); //b2
         }
 
-        private NDArray FeedForward(NDArray input)
+        private List<NDArray> FeedForward(NDArray input)
         {
-            //This method returns the output vector 
-            //"input" must be a InputSize x 1 matrix
-            NDArray info = input;
-            for (int i = 0; i < NumOfLayers; i++)
-            {
-                _layers[i] = ActivationFunction.Compute(info.dot(_weights[i]) + _biases[i]); //Layer = WEIGHTS * INPUT + BIASES
-                info = _layers[i];
-            }
-            
-            return ActivationFunction.Compute(info.dot(_weights.Last()) + _biases.Last()); //Output vector
+            var z1 = np.dot(X, _weights[0]) + _biases[0]; //Input * W1 + b1
+            var a = ActivationFunction.ComputeRelu(z1);
+            var z2 =  np.dot(X, _weights[1]) + _biases[1]; //Input * W2 + b2
+            var yPredict = ActivationFunction.ComputeSigmoid(z2);
+
+            return new List<NDArray> {z1, a, z2, yPredict};;
+        }
+
+        private NDArray BackPropagation(List<NDArray> forwardResults, NDArray input)
+        {
+            var z1 = forwardResults[0];
+            var a = forwardResults[1];
+            var z2 = forwardResults[2];
+            var yPredict = forwardResults[3];
+
+            var numExamples = input.shape[1];
+
+            var lost = (1 / numExamples) * np.sum(-Y * np.log(yPredict) - (1 - Y) * np.log(1 - yPredict));
+
+            //Calculating gradients
+            var dLdZ2 = yPredict - Y;
+            var dLdW2 = (1 / numExamples) * np.dot(dLdZ2, a.T);
+            var dLdb2 = (1 / numExamples) * np.sum(dLdZ2, 1, true);
+            var dLdZ1 = np.multiply(np.dot(_weights[1].T, dLdZ2), (1 - np.power(a, 2)));
+            var dLdW1 = (1 / numExamples) * np.dot(dLdZ1, X.T);
+            var dLdb1 = (1 / numExamples) * np.sum(dLdZ1, 1, true);
+
+            _weights[1] += dLdW2;
+            _biases[1] += dLdb2;
+            _weights[0] += dLdW1;
+            _biases[0] += dLdb1;
+
+            return lost;
         }
     }
 }
