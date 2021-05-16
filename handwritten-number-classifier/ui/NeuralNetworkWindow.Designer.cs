987ca8abe377aa19edac3481a6ebe06ccec07b1c
Sebastian Jaramillo <sebastian.jaramillo2@correo.icesi.edu.co>
2021-05-16 17:53:19 -0500

    Added the neural network window and button now is enabled

---
+++ handwritten-number-classifier/ui/NeuralNetworkWindow.Designer.cs
@@ -0,0 +1,125 @@
+﻿using System.ComponentModel;
+
+namespace handwritten_number_classifier.ui
+{
+    partial class NeuralNetworkWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Train = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.costLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize) (this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Train
+            // 
+            this.Train.Font = new System.Drawing.Font("Segoe UI Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.Train.Location = new System.Drawing.Point(106, 27);
+            this.Train.Name = "Train";
+            this.Train.Size = new System.Drawing.Size(310, 79);
+            this.Train.TabIndex = 0;
+            this.Train.Text = "Train";
+            this.Train.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Train.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.Location = new System.Drawing.Point(653, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 79);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Test";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 109);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(282, 263);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.Location = new System.Drawing.Point(335, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cost:";
+            // 
+            // costLbl
+            // 
+            this.costLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.costLbl.Location = new System.Drawing.Point(391, 131);
+            this.costLbl.Name = "costLbl";
+            this.costLbl.Size = new System.Drawing.Size(65, 37);
+            this.costLbl.TabIndex = 4;
+            // 
+            // NeuralNetworkWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 644);
+            this.Controls.Add(this.costLbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Train);
+            this.Name = "NeuralNetworkWindow";
+            this.Text = "NeuralNetworkWindow";
+            this.Load += new System.EventHandler(this.NeuralNetworkWindow_Load);
+            ((System.ComponentModel.ISupportInitialize) (this.chart1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label costLbl;
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label Train;
+
+        #endregion
+    }
+}
---
+++ handwritten-number-classifier/ui/NeuralNetworkWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+using handwritten_number_classifier.Model;
+
+namespace handwritten_number_classifier.ui
+{
+    public partial class NeuralNetworkWindow : Form
+    {
+
+        private Controller c;
+        public NeuralNetworkWindow(Controller c)
+        {
+            InitializeComponent();
+            this.c = c;
+        }
+
+        
+    }
+}
